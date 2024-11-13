e9a52cbf304967eb56ddbace5493027d77b4fbd7
yulychr <sandra_ch.r@hotmail.com>
2024-11-13 12:52:08 -0500

    feat: add AccountMs and CustomerMs microservices to the repository

---
+++ AccountMs/src/main/java/com/corebankingsystem/AccountMs/AccountMsApplication.java
@@ -0,0 +1,13 @@
+package com.corebankingsystem.AccountMs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountMsApplication.class, args);
+	}
+
+}
---
+++ AccountMs/src/test/java/com/corebankingsystem/AccountMs/AccountMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corebankingsystem.AccountMs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
